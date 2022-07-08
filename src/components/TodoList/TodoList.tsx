e6826852dd32c72aa3941eac065dd4d082cd6983
Alex Betke <arhontes@yandex.ru>
2022-07-08 12:07:57 +0300

    add type and callbacks to TodoList

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,72 +1,54 @@
 import React from 'react';
-import {FilterValuesType, TaskType} from "../../App";
 import UniButton from "../UniButton/UniButton";
 import s from './TodoList.module.css'
 import UniCheckBox from "../UniCheckbox/UniCheckBox";
 import AddItemForm from "../AddItemForm/AddItemForm";
 import EditableSpan from "../EditableSpan/EditableSpan";
-import {
-    changeTodoListFilterAC,
-    editTodoListTitleAC,
-    removeTodoListAC,
-    TodoListsReducerActionType
-} from "../reducers/TodoListsReducer";
-import {
-    addTaskAC,
-    changeTaskStatusAC,
-    editTaskTitleAC,
-    removeTaskAC, removeTasksListAC,
-    TasksReducerActionType
-} from "../reducers/TasksReducer";
+import {TasksType} from "../../AppWithRedux";
+import {FilterValuesType} from "../reducers/TodoListsReducer";
+import {TaskType} from "../reducers/TasksReducer";
 
 type TodoListPropsType = {
-    todoListDispatch: (action: TodoListsReducerActionType) => void
-    tasksDispatch:(action:TasksReducerActionType)=>void
     todoListID: string
     title: string
-    tasks: TaskType[]
+    tasks: TasksType
     filter: FilterValuesType | undefined
+    addTask: (todoListID: string, title: string) => void
+    changeTaskStatus: (todolistId: string, t: string, el: boolean) => void
+    editTaskTitle: (todoListID: string, taskID: string, title: string) => void
+    removeTask: (todoListID: string, taskID: string) => void
+    editTodoListTitle: (todoListID: string, title: string) => void
+    removeTodoList: (todoListID: string) => void
+    changeTodoListFilter: (todoListID: string, value: FilterValuesType) => void
 }
 
 export const TodoList = (props: TodoListPropsType) => {
 
-
-    const changeTodoListFilterHandler = (value: FilterValuesType) => {
-        props.todoListDispatch(changeTodoListFilterAC(props.todoListID, value))
-    }
-    const editTodoListTitleHandler = (title: string) => {
-        props.todoListDispatch(editTodoListTitleAC(props.todoListID, title))
-    }
-    const removeTodoListHandler = ()=>{
-        props.todoListDispatch(removeTodoListAC(props.todoListID))
-        props.tasksDispatch(removeTasksListAC(props.todoListID))
-    }
-    const addTaskHandler = (title: string) => {
-        const taskTitle = title.trim()
-        if (!taskTitle) {
-            return
+    function getTasksByTodoListFilter(): Array<TaskType> {
+        let filteredTasks
+        switch (props.filter) {
+            case "completed":
+                filteredTasks = props.tasks[props.todoListID].filter((t) => t.isDone)
+            case "active":
+                filteredTasks=  props.tasks[props.todoListID].filter((t) => !t.isDone)
+            case "all":
+                filteredTasks= [...props.tasks[props.todoListID]]
+            default:
+                filteredTasks= [...props.tasks[props.todoListID]]
         }
-        props.tasksDispatch(addTaskAC(props.todoListID, taskTitle))
+        return filteredTasks
     }
-    const editTaskTitleHandler = (taskID: string, title: string) => {
-        props.tasksDispatch(editTaskTitleAC(props.todoListID, taskID, title))
-    }
-    const changeTaskStatusHandler = (todolistId: string, t: string, el: boolean) => {
-        props.tasksDispatch(changeTaskStatusAC(todolistId, t, el))
-    }
-    const removeTaskHandler = (taskID:string)=>{
-        props.tasksDispatch(removeTaskAC(props.todoListID,taskID))
-    }
+    const filteredTasks = getTasksByTodoListFilter()
 
-    const tasks = props.tasks.length
-        ? props.tasks.map(t =>
+
+    const tasks = filteredTasks.length ? filteredTasks.map(t =>
             <li key={t.id}>
-                <UniCheckBox onChangeChecked={(checked) => changeTaskStatusHandler(props.todoListID, t.id, checked)}
+                <UniCheckBox onChangeChecked={(checked) => props.changeTaskStatus(props.todoListID, t.id, checked)}
                              checked={t.isDone}/>
-                <EditableSpan title={t.title} callback={(title) => editTaskTitleHandler(t.id, title)}/>
+                <EditableSpan title={t.title} callback={(title) => props.editTaskTitle(props.todoListID, t.id, title)}/>
 
                 <UniButton value={props.filter} children={"X"}
-                           onClick={() => removeTaskHandler(t.id)}/>
+                           onClick={() => props.removeTask(props.todoListID, t.id)}/>
             </li>)
 
         : <div> No tasks here</div>
@@ -74,25 +56,28 @@ export const TodoList = (props: TodoListPropsType) => {
         <div className={s.todo}>
 
             <div className={s.smartTitle}>
-                <EditableSpan callback={editTodoListTitleHandler} title={props.title}/>
-                <UniButton onClick={removeTodoListHandler}>-</UniButton>
+                <EditableSpan callback={(title) => props.editTodoListTitle(props.todoListID, title)}
+                              title={props.title}/>
+                <UniButton onClick={() => props.removeTodoList(props.todoListID)}>-</UniButton>
             </div>
 
-            <AddItemForm callBack={addTaskHandler}/>
+            <AddItemForm callBack={(title) => props.addTask(props.todoListID, title)}/>
 
             <ul>{tasks}</ul>
 
             <div className={s.filterButtonArea}>
                 <UniButton className={props.filter === 'all' ? s.activeFilter : ""}
-                           onClick={()=>changeTodoListFilterHandler( 'all')} children={'all'}/>
+                           onClick={() => props.changeTodoListFilter(props.todoListID, 'all')} children={'all'}/>
 
                 <UniButton className={props.filter === 'active' ? s.activeFilter : ""}
-                           onClick={()=>changeTodoListFilterHandler( "active")} children={"active"}/>
+                           onClick={() => props.changeTodoListFilter(props.todoListID, "active")} children={"active"}/>
 
                 <UniButton className={props.filter === 'completed' ? s.activeFilter : ""}
-                           onClick={()=>changeTodoListFilterHandler( 'completed')} children={"completed"}/>
+                           onClick={() => props.changeTodoListFilter(props.todoListID, 'completed')}
+                           children={"completed"}/>
             </div>
         </div>
     )
+
 }
 
